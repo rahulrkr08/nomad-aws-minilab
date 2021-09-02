ad3fbe3ba4b96ee81f412ce6a1e6e1c319e34eb5
Rahul Radhakrishnan <rahul_radhakrishnan@condenast.com>
2021-09-02 18:18:11 +0530

    updated spot options

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "nomad-node" {
     count = var.nomad_node_count
     ami = var.nomad_node_ami_id
-    spot_price    = var.nomad_node_spot_price
     instance_type = var.nomad_node_instance_size
     key_name = var.aws_key_name
     subnet_id = aws_subnet.nomad-lab-pub[count.index].id
@@ -9,6 +8,14 @@ resource "aws_instance" "nomad-node" {
     associate_public_ip_address = true
     user_data = file("conf/install-nomad.sh")
     private_ip = "10.0.${count.index}.100"
+
+    instance_market_options {
+        market_type = "spot"
+        spot_options {
+            max_price  = var.nomad_node_spot_price
+            instance_interruption_behavior = "stop"
+        }
+    }
 
     tags = {
         Terraform = "true"
