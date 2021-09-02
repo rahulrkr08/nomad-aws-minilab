bbee87a60e5d4cce85c39319c36fa83da04078df
Rahul Radhakrishnan <rahul_radhakrishnan@condenast.com>
2021-09-02 19:34:07 +0530

    updated ec2

--- main.tf
+++ main.tf
@@ -23,11 +23,16 @@ resource "aws_launch_template" "nomad-node" {
   image_id                    = var.nomad_node_ami_id
   instance_type               = var.nomad_node_instance_size
   key_name                    = var.aws_key_name
-  subnet_id                   = aws_subnet.nomad-lab-pub[count.index].id
+  
+  network_interfaces {
+    associate_public_ip_address = true
+    subnet_id                   = aws_subnet.nomad-lab-pub[count.index].id
+    private_ip_address          = "10.0.${count.index}.100"
+  }
+  
   vpc_security_group_ids      = [aws_security_group.nomad-sg.id]
-  associate_public_ip_address = true
   user_data                   = file("conf/install-nomad.sh")
-  private_ip_address          = "10.0.${count.index}.100"
+  
 
   tags = {
     Terraform = "true"
