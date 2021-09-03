85c5a93a49d67c48d274c2b712dc7cde8ce672ab
Rahul Radhakrishnan <rahulrkr@hotmail.com>
2021-09-03 19:31:50 +0530

    Removed network

--- modules/vpc/network.tf
+++ modules/vpc/network.tf
@@ -1,40 +1,40 @@
-resource "aws_internet_gateway" "nomad-lab-igw" {
-  vpc_id = var.vpc_id
+# resource "aws_internet_gateway" "nomad-lab-igw" {
+#   vpc_id = var.vpc_id
 
-  tags = {
-    Name = "nomad-lab"
-    Terraform = "true"
-    Avaazz = "true"
-  }
-}
+#   tags = {
+#     Name = "nomad-lab"
+#     Terraform = "true"
+#     Avaazz = "true"
+#   }
+# }
 
-resource "aws_route_table" "nomad-lab-public-crt" {
-  vpc_id = var.vpc_id
+# resource "aws_route_table" "nomad-lab-public-crt" {
+#   vpc_id = var.vpc_id
   
-  route {
-      cidr_block = "0.0.0.0/0" 
-      gateway_id = aws_internet_gateway.nomad-lab-igw.id
-  }
+#   route {
+#       cidr_block = "0.0.0.0/0" 
+#       gateway_id = aws_internet_gateway.nomad-lab-igw.id
+#   }
   
-  tags = {
-    Name = "nomad-lab"
-    Terraform = "true"
-    Avaazz = "true"
-  }
-}
+#   tags = {
+#     Name = "nomad-lab"
+#     Terraform = "true"
+#     Avaazz = "true"
+#   }
+# }
 
-resource "aws_route_table_association" "subnet_association" {
-  count = length(data.aws_subnet_ids.subnet_ids.ids)
+# resource "aws_route_table_association" "subnet_association" {
+#   count = length(data.aws_subnet_ids.subnet_ids.ids)
 
-  subnet_id = sort(data.aws_subnet_ids.subnet_ids.ids)[count.index]
-  route_table_id = aws_route_table.nomad-lab-public-crt.id
+#   subnet_id = sort(data.aws_subnet_ids.subnet_ids.ids)[count.index]
+#   route_table_id = aws_route_table.nomad-lab-public-crt.id
 
-  lifecycle { 
-      create_before_destroy = true 
-  }
+#   lifecycle { 
+#       create_before_destroy = true 
+#   }
 
-  depends_on = [
-    aws_route_table.nomad-lab-public-crt,
-  ]
-}
+#   depends_on = [
+#     aws_route_table.nomad-lab-public-crt,
+#   ]
+# }
 
