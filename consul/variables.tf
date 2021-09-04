e7a19b72c550c6d4d870810b52f6cc081d76e8d6
Rahul Radhakrishnan <rahulrkr@hotmail.com>
2021-09-04 10:53:19 +0530

    new begining

---
+++ consul/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "subnet_id" {}
+# variable "azs" {}
+variable "vpc_cidr" {}
+variable "vpc_id" {}
+# variable "bastion_connect" {}
---
+++ infrastructure.tf
@@ -0,0 +1,63 @@
+# This creates a bastion host
+# module "bastion" {
+#   source = "./bastion"
+#   public_subnet             = aws_subnet.public.id
+#   ami                       = var.base_ec2_ami
+#   instance_type             = "t2.micro"
+#   key_name                  = "tutorialinux"
+#   vpc_id                    = aws_vpc.tutorialinux.id
+# }
+
+# This creates a consul cluster
+module "consul" {
+  source = "./consul"
+  ami                       = var.base_ec2_ami
+  instance_type             = "t2.micro"
+  # azs                       = "us-west-2a,us-west-2b,us-west-2c"
+  key_name                  = "tutorialinux"
+  # name MUST be consul-server so that consul auto-join works (instances will get a corresponding 'role' tag)
+  name                      = "consul-server"
+  subnet_id                 = aws_subnet.private.id
+  vpc_id                    = aws_vpc.tutorialinux.id
+  vpc_cidr                  = aws_vpc.tutorialinux.cidr_block
+  # bastion_connect           = module.bastion.bastion_public_ip
+}
+
+# This creates a nomad cluster
+module "nomad" {
+  source = "./nomad"
+  ami                       = var.base_ec2_ami
+  instance_type             = "t2.micro"
+  # azs                       = "us-west-2a,us-west-2b,us-west-2c"
+  nomad_cluster_size        = 3
+  key_name                  = "tutorialinux"
+  name                      = "nomad-server"
+  subnet_id                 = aws_subnet.private.id
+  vpc_id                    = aws_vpc.tutorialinux.id
+  vpc_cidr                  = aws_vpc.tutorialinux.cidr_block
+}
+
+# This creates a traefik host
+# module "traefik" {
+#   source = "./traefik"
+#   ami                       = var.base_ec2_ami
+#   instance_type             = "t2.micro"
+#   # azs                       = "us-west-2a,us-west-2b,us-west-2c"
+#   num_instances             = 3
+#   key_name                  = "tutorialinux"
+#   name                      = "traefik"
+#   public_subnet             = aws_subnet.public.id
+#   vpc_id                    = aws_vpc.tutorialinux.id
+#   vpc_cidr                  = aws_vpc.tutorialinux.cidr_block
+# }
+
+# This instantiates an nginx host, running the consul agent and reading from the consul KV store
+# switched off for now, to pursue traefik/nomad-service stuff
+// module "nginx" {
+//   source = "./nginx"
+//   ami                       = var.base_ec2_ami
+//   instance_type             = "t2.micro"
+//   key_name                  = "tutorialinux"
+//   subnet_id                 = aws_subnet.public.id
+//   vpc_id                    = aws_vpc.tutorialinux.id
+// }
