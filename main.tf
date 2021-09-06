e2518f363060024b4c1fef91fb2cf120273f4ae5
Rahul Radhakrishnan <rahulrkr@hotmail.com>
2021-09-06 15:03:33 +0530

    change to spot instance

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   access_key      = var.aws_access_key
   secret_key      = var.aws_secret_key
   region          = var.aws_region
+
+  tags = merge(
+    var.additional_tags,
+    {}
+  )
 }
 
 terraform {
