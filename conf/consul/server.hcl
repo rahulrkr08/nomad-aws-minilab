c8c397fb329cd636e5dd6110ff6387ee9346a905
Rahul Radhakrishnan <rahulrkr@hotmail.com>
2021-09-06 15:44:02 +0530

    change to spot instance

--- conf/consul/server.hcl
+++ conf/consul/server.hcl
@@ -1,12 +1,12 @@
 data_dir = "/tmp/consul/server"
 
 server           = true
-bootstrap_expect = 1
+bootstrap_expect = 3
 advertise_addr   = "{{ GetInterfaceIP `eth0` }}"
 client_addr      = "0.0.0.0"
 ui               = true
 datacenter       = "dc-aws-1"
-"retry_join"     = ["provider=aws tag_key=Nomad tag_value=true"]
+retry_join       = ["provider=aws tag_key=Nomad tag_value=true addr_type=private_v4"]
 retry_max        = 10
 retry_interval   = "15s"
 
@@ -15,3 +15,8 @@ acl = {
   default_policy = "allow"
   enable_token_persistence = true
 }
+
+auto_advertise = true
+
+server_auto_join = true
+client_auto_join = true
--- modules/nomad-cluster/asg.tf
+++ modules/nomad-cluster/asg.tf
@@ -1,20 +1,20 @@
-resource "aws_spot_instance_request" "nomad-node" {
-    count = var.nomad_node_count
-    ami = var.nomad_node_ami_id
-    spot_price = var.nomad_node_spot_price
-    instance_type = var.nomad_node_instance_size
-    key_name = var.aws_key_name
-    subnet_id = var.subnets[count.index] # aws_subnet.nomad-lab-pub[count.index].id
-    vpc_security_group_ids = var.security_groups
-    associate_public_ip_address = true
-    user_data = file("conf/install-nomad.sh")
-    private_ip = "10.0.${count.index}.100"
+# resource "aws_spot_instance_request" "nomad-node" {
+#     count = var.nomad_node_count
+#     ami = var.nomad_node_ami_id
+#     spot_price = var.nomad_node_spot_price
+#     instance_type = var.nomad_node_instance_size
+#     key_name = var.aws_key_name
+#     subnet_id = var.subnets[count.index] # aws_subnet.nomad-lab-pub[count.index].id
+#     vpc_security_group_ids = var.security_groups
+#     associate_public_ip_address = true
+#     user_data = file("conf/install-nomad.sh")
+#     private_ip = "10.0.${count.index}.100"
 
-    tags = merge(
-        var.additional_tags,
-        {}
-    )
-}
+#     tags = merge(
+#         var.additional_tags,
+#         {}
+#     )
+# }
 
 # data "aws_iam_policy_document" "ecs-instance-policy" {
 #   statement {
@@ -53,51 +53,51 @@ resource "aws_spot_instance_request" "nomad-node" {
 #   }
 # }
 
-# resource "aws_iam_role" "nomad-node-role" {
-#   name_prefix         = "nomad-node-role-"
-#   path                = "/ecs/"
-#   assume_role_policy  = data.aws_iam_policy_document.ecs-instance-policy.json
-# }
+resource "aws_iam_role" "nomad-node-role" {
+  name_prefix         = "nomad-node-role-"
+  path                = "/ecs/"
+  assume_role_policy  = data.aws_iam_policy_document.ecs-instance-policy.json
+}
 
-# resource "aws_iam_instance_profile" "nomad-node-profile" {
-#   name_prefix = "nomad-node-profile-"
-#   role = aws_iam_role.nomad-node-role.name
-# }
+resource "aws_iam_instance_profile" "nomad-node-profile" {
+  name_prefix = "nomad-node-profile-"
+  role = aws_iam_role.nomad-node-role.name
+}
 
-# resource "aws_launch_configuration" "nomad-node-lc" {
-#   name_prefix                 = "nomad-launch-configuration-"
-#   image_id                    = var.nomad_node_ami_id
-#   instance_type               = var.nomad_node_instance_size
-#   spot_price                  = var.nomad_node_spot_price
-#   key_name                    = var.aws_key_name
-#   security_groups             = var.security_groups
-#   # user_data                   = data.template_file.nomad_server_userdata.rendered
-#   user_data                   = file("conf/install-nomad.sh")
+resource "aws_launch_configuration" "nomad-node-lc" {
+  name_prefix                 = "nomad-launch-configuration-"
+  image_id                    = var.nomad_node_ami_id
+  instance_type               = var.nomad_node_instance_size
+  spot_price                  = var.nomad_node_spot_price
+  key_name                    = var.aws_key_name
+  security_groups             = var.security_groups
+  # user_data                   = data.template_file.nomad_server_userdata.rendered
+  user_data                   = file("conf/install-nomad.sh")
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# module "asg" {
-#   source  = "terraform-aws-modules/autoscaling/aws"
-#   version                     = "~> 4.4.0"
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version                     = "~> 4.4.0"
   
-#   name                        = "nomad-node-asg"
+  name                        = "nomad-node-asg"
 
-#   # Launch configuration
-#   create_lc                   = false
-#   use_lc                      = true
-#   launch_configuration        = aws_launch_configuration.nomad-node-lc.name
-#   iam_instance_profile_arn    = aws_iam_instance_profile.nomad-node-profile.id
+  # Launch configuration
+  create_lc                   = false
+  use_lc                      = true
+  launch_configuration        = aws_launch_configuration.nomad-node-lc.name
+  iam_instance_profile_arn    = aws_iam_instance_profile.nomad-node-profile.id
 
-#   # Auto scaling group 
-#   vpc_zone_identifier         = [for s in var.subnets : s]
-#   health_check_type           = "EC2"
-#   min_size                    = 1
-#   max_size                    = var.nomad_node_count
-#   desired_capacity            = var.nomad_node_count
-#   wait_for_capacity_timeout   = 0
+  # Auto scaling group 
+  vpc_zone_identifier         = [for s in var.subnets : s]
+  health_check_type           = "EC2"
+  min_size                    = 1
+  max_size                    = var.nomad_node_count
+  desired_capacity            = var.nomad_node_count
+  wait_for_capacity_timeout   = 0
 
 #   network_interfaces = [
 #     {
@@ -117,8 +117,8 @@ resource "aws_spot_instance_request" "nomad-node" {
 #     }
 #   ]
 
-#   tags_as_map = merge(
-#     var.additional_tags,
-#     {}
-#   )
-# }
+  tags_as_map = merge(
+    var.additional_tags,
+    {}
+  )
+}
